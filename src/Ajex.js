d6a172975133dfed36b0873a40173466c80b86e6
nolo lefatle <lehlohonolo.lefatle@umuzi.org>
2020-05-04 14:25:51 +0200

    addnew visiter registers correctly on database.

--- src/Ajex.js
+++ src/Ajex.js
@@ -1,12 +1,22 @@
-var request = new Request('http://localhost:1221/addNewVisitor', {
-	method: 'POST', 
-	headers: new Headers({
-		'Content-Type': 'application/json'
-    }),
-    body: JSON.stringify(new URLSearchParams(new FormData(document.getElementById("visitorForm"))))
-});
+var form = document.getElementById("visitorForm");
+let fields = ["visitorname", "assistantname", "visitorage","visitdate", "visittime", "comments"];
 
-function addVisit(e) {
+
+function addVisit() {
+
+    let data = new Object();
+
+    for (let i = 0; i < form.elements.length -1; i++) {
+        data[fields[i]] = form.elements[i].value;
+    }
+
+    var request = new Request('http://localhost:1221/addNewVisitor', {
+        method: 'POST', 
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        }),
+        body: JSON.stringify(data)
+    });
 
     fetch(
         request
